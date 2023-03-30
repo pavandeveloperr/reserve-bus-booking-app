b358f73c9ca76d91642d408a0624e57d975b9d24
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-03-30 11:41:27 +0530

    feat : auth routes

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-const express = require('express');
-const colors = require('colors');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db.js');
+// packages imports
+const express = require("express");
+require("express-async-errors");
+const colors = require("colors");
+const morgan = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
+// files imports
+const connectDB = require("./config/db.js");
+// routes
+const testRoutes = require("./routes/testRoute");
+const authRoutes = require("./routes/authRoutes");
+const errorMiddleware = require("./middlewares/errorMiddleware.js");
 
 // dotenv config
 dotenv.config();
@@ -15,19 +23,23 @@ const app = express();
 
 // middlewares
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(cors());
+app.use(morgan("dev"));
 
 // routes
-app.get('/', (req, res) => {
-    res.status(200).send({
-        message: "server running",
-    });
-});
+app.use("/api/v1/test", testRoutes);
+app.use("/api/v1/auth", authRoutes);
+
+// validation middleware
+app.use(errorMiddleware);
 
 // port
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 // Listen port
 app.listen(port, () => {
-    console.log(`Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`.bgGreen.white)
-})
+  console.log(
+    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+      .bgGreen.white
+  );
+});
